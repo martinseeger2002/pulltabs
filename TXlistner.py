1f351f1f2ac0724fec9d1883984954345528e212
₿ig Chief Energy <57598081+martinseeger2002@users.noreply.github.com>
2024-06-24 18:10:31 -0500

    Update TXlistner.py

--- TXlistner.py
+++ TXlistner.py
@@ -5,10 +5,10 @@ import time
 from datetime import datetime
 
 # Configuration
-api_key = ""
+api_key = "84a74543a2ac4ac1993bd77e38acb312"
 wallet_address = "DDwPZVz1TRn9gAcXoFP2XuryvW72kyDZqq"
 base_api_url = f"https://api.blockcypher.com/v1/doge/main/addrs/{wallet_address}/full?token={api_key}"
-check_interval = 500  # Check interval in seconds
+check_interval = 300  # Check interval in seconds
 max_additional_calls = 1  # Maximum number of additional API calls
 
 # File paths
@@ -19,14 +19,14 @@ def get_transactions(api_url):
     print("Fetching transactions from API...")
     transactions = []
     api_calls = 0
-    while api_url and api_calls <= max_additional_calls:
+    while api_url and api_calls < max_additional_calls:
         response = requests.get(api_url)
         api_calls += 1
         if response.status_code == 200:
             response_json = response.json()
             transactions.extend(response_json.get('txs', []))
             print(f"Fetched {len(response_json.get('txs', []))} transactions.")
-            if response_json.get('hasMore') and api_calls <= max_additional_calls:
+            if response_json.get('hasMore') and api_calls < max_additional_calls:
                 api_url = f"{base_api_url}&before={transactions[-1]['block_height']}"
                 print("More transactions available, fetching next batch...")
             else:
@@ -76,15 +76,18 @@ def update_tab_que(new_transactions):
     print("tabQue.json updated successfully.")
 
 def main():
+    processed_hashes = set()
+    
     while True:
         print("Starting new transaction check cycle...")
         existing_tx_hashes = parse_log()
+        processed_hashes.update(existing_tx_hashes)
         transactions, api_calls = get_transactions(base_api_url)
         
         new_transactions = []
         for tx in transactions:
             tx_hash = tx["hash"]
-            if tx_hash not in existing_tx_hashes:
+            if tx_hash not in processed_hashes:
                 sender_address = tx["addresses"][0] if "addresses" in tx and tx["addresses"] else "Unknown"
                 value = sum(output["value"] for output in tx["outputs"] if wallet_address in output["addresses"])
                 new_transactions.append({
@@ -93,6 +96,7 @@ def main():
                     "sender": sender_address,
                     "confirmed": tx.get("confirmed", datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"))
                 })
+                processed_hashes.add(tx_hash)
         
         new_transactions.sort(key=lambda x: x['confirmed'])
         
